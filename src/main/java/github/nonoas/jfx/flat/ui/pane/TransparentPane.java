dcd30193bf453e3e4cbe1039d0e310325cf4db5e
Nonoas <1356955215@qq.com>
2025-07-04 17:59:50 +0800

    优化界面界面布局

--- src/main/java/github/nonoas/jfx/flat/ui/pane/TransparentPane.java
+++ src/main/java/github/nonoas/jfx/flat/ui/pane/TransparentPane.java
@@ -2,8 +2,6 @@ package github.nonoas.jfx.flat.ui.pane;
 
 import github.nonoas.jfx.flat.ui.common.InsetConstant;
 import github.nonoas.jfx.flat.ui.utils.UIUtil;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -11,14 +9,12 @@ import javafx.scene.Node;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 
 /**
- * 透明面板
+ * 透明面板（优化版，使用 StackPane 替代 VBox）
  *
  * @author Nonoas
  * @datetime 2021/12/4 15:42
@@ -26,14 +22,14 @@ import javafx.scene.shape.Rectangle;
 public class TransparentPane extends AnchorPane {
 
     /**
-     * 内容布局,实际显示节点的布局面板
+     * 内容布局, 实际显示节点的布局面板
      */
     private final AnchorPane contentPane = new AnchorPane();
 
     /**
      * 阴影布局，用于生成阴影
      */
-    private final VBox shadowPane = new VBox();
+    private final StackPane shadowPane = new StackPane();
 
     /**
      * 按钮布局
@@ -43,9 +39,7 @@ public class TransparentPane extends AnchorPane {
     private final Insets ROOT_PADDING = new Insets(InsetConstant.SHADOW_SIZE_1);
 
     public TransparentPane() {
-
         this.setPadding(ROOT_PADDING);
-
         setStyle("-fx-background-color: transparent !important;");
 
         initShadowPane();
@@ -74,13 +68,7 @@ public class TransparentPane extends AnchorPane {
     }
 
     private void initContentPane() {
-        VBox.setVgrow(contentPane, Priority.ALWAYS);
-        Rectangle clip = new Rectangle();
-        widthProperty().addListener((ov, old, newVal) -> clip.setWidth((Double) newVal - getPadding().getLeft() - 1));
-        heightProperty().addListener((ov, old, newVal) -> clip.setHeight((Double) newVal - getPadding().getTop() - 1));
-        clip.setArcHeight(60);
-        clip.setArcWidth(60);
-        contentPane.setClip(clip);
+        // 不需要 VBox.setVgrow 了，StackPane 会自动让它填满
     }
 
     public ObservableList<Node> getSysButtons() {
@@ -97,9 +85,10 @@ public class TransparentPane extends AnchorPane {
      * @param content 根布局
      */
     public void setContent(Node content) {
-        VBox.setVgrow(content, Priority.ALWAYS);
         if (content instanceof Region region) {
             UIUtil.setAnchor(region, 0);
+            region.prefWidthProperty().bind(contentPane.widthProperty());
+            region.prefHeightProperty().bind(contentPane.heightProperty());
             contentPane.getChildren().setAll(region);
         } else {
             contentPane.getChildren().setAll(content);
@@ -107,15 +96,10 @@ public class TransparentPane extends AnchorPane {
     }
 
     private DropShadow getDropShadow() {
-        // 阴影向外
         DropShadow dropshadow = new DropShadow();
-        // 颜色蔓延的距离
         dropshadow.setRadius(15);
-        // 颜色变淡的程度
         dropshadow.setSpread(0.15);
-        // 设置颜色
         dropshadow.setColor(Color.rgb(0, 0, 0, 0.3));
         return dropshadow;
     }
-
 }
